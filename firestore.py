de9a53b559032bab501c4fad0fc22073dbc4eecb
allocarodoti <126686075+KennethM19@users.noreply.github.com>
2025-10-04 16:46:22 -0500

    fix: fixed conection firestore

--- firestore.py
+++ firestore.py
@@ -1,7 +1,10 @@
+import json
+
 import firebase_admin
 from firebase_admin import credentials, firestore
 import config
 
-cred = credentials.Certificate(config.FIREBASE_CREDENTIALS)
+cred_dict = json.loads(config.FIREBASE_CREDENTIALS)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred, {})
 db_firestore = firestore.client()
