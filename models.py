9ce0da2a8e63d6d25ff0ae7e49a4a5a9579ae46a
allocarodoti <126686075+KennethM19@users.noreply.github.com>
2025-10-08 17:28:49 -0500

    feat: init backend

---
+++ models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class Role(Base):
+    __tablename__ = 'role'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    users = relationship('Users', back_populates='role')
+
+class TypeDocument(Base):
+    __tablename__ = 'typedocument'
+    id = Column(Integer, primary_key=True)
+
+    users = relationship('Users', back_populates='type_document')
+
+class Users(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    tipo_document_id = Column(Integer, ForeignKey('typedocument.id'), nullable=False)
+    num_document = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    birth_date = Column(Date, nullable=False)
+    address = Column(String, nullable=False)
+    num_cellphone = Column(String, nullable=True)
+    num_telephone = Column(String, nullable=True)
+    email = Column(String, unique=True)
+    password = Column(String, nullable=False)
+    role_id = Column(Integer, ForeignKey('role.id'), nullable=False)
+
+    type_document = relationship('TypeDocument', back_populates='users')
+    role = relationship('Role', back_populates='users')
+
+
